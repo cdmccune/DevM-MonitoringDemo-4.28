5ec202f2b5770d27c5857fd71dc1b4ef28564d49
cdmccune <curtmccune@gmail.com>
2022-04-28 13:29:46 -0700

    added endpoint to css

--- server/index.js
+++ server/index.js
@@ -17,6 +17,12 @@ rollbar.log('Hello world!')
 
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
+    rollbar.info('file served')
+})
+
+app.get('/style', (req,res) => {
+    res.sendFile(path.join(__dirname, '../index.html'))
+    rollbar.info('css file served')
 })
 
 const port = process.env.PORT || 4545

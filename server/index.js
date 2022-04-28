3c4260ee6a9f500648679cae54cc38272241df61
cdmccune <curtmccune@gmail.com>
2022-04-28 11:46:44 -0700

    added functionaility and rollbar tracking

--- server/index.js
+++ server/index.js
@@ -3,6 +3,19 @@ const path = require("path")
 
 const app = express()
 
+let students = []
+
+app.post('/api/student', (req, res)=>{
+    let {name} = req.body
+    name = name.trim()
+
+    students.push(name)
+
+    rollbar.log('Student added successfully', {author: 'Scott', type: 'manual entry'})
+    
+    res.status(200).send(students)
+})
+
 // include and initialize the rollbar library with your access token
 var Rollbar = require('rollbar')
 var rollbar = new Rollbar({
@@ -20,5 +33,7 @@ app.get('/', (req,res) => {
 })
 
 const port = process.env.PORT || 4545
+
+app.use(rollbar.errorHandler())
 
 app.listen(port, () => {console.log(`Take us to warp ${port}`)})
